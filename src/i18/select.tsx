7b71cd28bdac2c5950977851d6c3d8e1a9dd1952
Marco Levrero <marco.levrero@gmail.com>
2022-01-09 17:16:19 +0100

    Stop propagation i18n selector

--- src/i18/select.tsx
+++ src/i18/select.tsx
@@ -1,12 +1,16 @@
-import { Button } from '@geist-ui/react'
+import { Button, Spacer } from '@geist-ui/react'
 import { Gb, It } from './icons'
 import { useTranslation } from 'react-i18next'
 import { Locale } from './i18n'
+import React from 'react'
 
 export const SelectI18N = () => {
   const { i18n } = useTranslation()
 
-  const handler = (l: Locale) => i18n.changeLanguage(l)
+  const handler = (e: React.MouseEvent<HTMLButtonElement>, l: Locale) => {
+    e.stopPropagation()
+    i18n.changeLanguage(l)
+  }
   const isActive = (l: Locale) => i18n.language === l
 
   return (
@@ -14,14 +18,15 @@ export const SelectI18N = () => {
       <Button
         width="50px"
         disabled={isActive('it')}
-        onClick={() => handler('it')}
+        onClick={e => handler(e, 'it')}
       >
         <It />
       </Button>
+      <Spacer />
       <Button
         width="50px"
         disabled={isActive('gb')}
-        onClick={() => handler('gb')}
+        onClick={e => handler(e, 'gb')}
       >
         <Gb />
       </Button>
--- src/main/header/mobile/MobileDrawerContent.tsx
+++ src/main/header/mobile/MobileDrawerContent.tsx
@@ -1,4 +1,4 @@
-import { Grid } from '@geist-ui/react'
+import {  Grid } from '@geist-ui/react'
 import { SelectI18N } from '../../../i18/select'
 import { Gifts } from '../links/Gifts'
 import { Home } from '../links/Home'
@@ -8,6 +8,9 @@ import { Information } from '../links/Information'
 export const MobileDrawerContent = () => {
   return (
     <Grid.Container gap={3} justify="center" width="100%">
+      <Grid xs={24}>
+        <SelectI18N />
+      </Grid>
       <Grid xs={24}>
         <Home />
       </Grid>
@@ -19,9 +22,6 @@ export const MobileDrawerContent = () => {
       </Grid>
       <Grid xs={24}>
         <Gifts />
-      </Grid>
-      <Grid xs={24}>
-        <SelectI18N />
       </Grid>
     </Grid.Container>
   )
