827107da7342b7855eaac22d22fd2a6bfa59bbe5
Marco Levrero <marco.levrero@gmail.com>
2022-01-30 17:21:29 +0100

    show loader on submit

--- src/main/pages/RSVP.tsx
+++ src/main/pages/RSVP.tsx
@@ -1,29 +1,50 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Input as GInput, Button, Grid, Spacer } from '@geist-ui/react'
+import {
+  Input as GInput,
+  Button,
+  Grid,
+  Spacer,
+  Modal,
+  useModal,
+} from '@geist-ui/react'
 import { findPartyByCode } from '../http/api'
+import { loadParNWithTimeout } from '../loader/utils'
+import { Loader } from '../loader/Loader'
+import { fold } from 'fp-ts/Option'
 
 export const RSVP = (props: { mobileView: boolean }) => {
   useEffect(() => document.body.classList.remove('with-background'))
 
-  const navigate = useNavigate()
-
   const Input = GInput as any // ???
 
+  const navigate = useNavigate()
+  const { setVisible, bindings } = useModal()
   const [state, setState] = useState('')
 
   const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
     setState(e.target.value.toUpperCase())
 
-  const onSubmit = () =>
-    findPartyByCode(state.toLowerCase()).then(p => {
-      navigate(`./${state.toLowerCase()}`, {
-        state: {
-          rsvp: { type: 'asked', value: p },
-          mobileView: props.mobileView,
-        },
-      })
-    })
+  const onSubmit = () => {
+    setVisible(true)
+
+    loadParNWithTimeout(() => findPartyByCode(state.toLowerCase()), 1500, 5000)
+      .then(
+        fold(
+          () => navigate('/rsvp/error'),
+          p => {
+            navigate(`./${state.toLowerCase()}`, {
+              state: {
+                rsvp: { type: 'asked', value: p },
+                mobileView: props.mobileView,
+              },
+            })
+          },
+        ),
+      )
+      .catch(_ => navigate('/rsvp/error'))
+      .finally(() => setVisible(false))
+  }
 
   return (
     <>
@@ -45,6 +66,12 @@ export const RSVP = (props: { mobileView: boolean }) => {
         </Grid>
         <Spacer h={1} />
       </Grid.Container>
+
+      <Modal {...bindings} disableBackdropClick={true} keyboard={false}>
+        <Modal.Content>
+          <Loader />
+        </Modal.Content>
+      </Modal>
     </>
   )
 }
