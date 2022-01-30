e6a0c9ea8f5e6b6811bed650084a90434e88a2db
Marco Levrero <marco.levrero@gmail.com>
2022-01-30 20:05:12 +0100

    remove display

--- src/main/logo/Logo.tsx
+++ src/main/logo/Logo.tsx
@@ -1,8 +1,6 @@
 import logo from '../../resources/logo.png'
-import { Display, Image } from '@geist-ui/react'
+import { Image } from '@geist-ui/react'
 
 export const Logo = (props: { width: number }) => (
-  <Display>
-    <Image width={props.width} src={logo} />
-  </Display>
+  <Image width={props.width} src={logo} />
 )
