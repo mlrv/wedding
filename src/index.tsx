c14197d0be7379fd960eaf6c7e36db9f19da5af7
Marco Levrero <marco.levrero@gmail.com>
2022-01-30 21:31:49 +0100

    switch to hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import './index.css'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './main/App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 )
