03c2ed8af318ae69a3c6b2560270106dc4ef19a3
Marco Levrero <marco.levrero@gmail.com>
2022-02-03 21:10:45 +0100

    rsvp error page

--- src/main/pages/RSVPError.tsx
+++ src/main/pages/RSVPError.tsx
@@ -1,5 +1,36 @@
-import { Text } from '@geist-ui/react'
+import { Grid, Link, Spacer, Text } from '@geist-ui/react'
+import { useTranslation } from 'react-i18next'
 
 export const RSVPError = () => {
-  return <Text>Error</Text>
+  const email = 'oliviaemarcowedding@email.com'
+  const { t } = useTranslation()
+
+  return (
+    <Grid.Container
+      style={{ alignItems: 'center', flexDirection: 'column' }}
+    >
+      <Grid xs={24}>
+        <Text font="25px" className="font-standard">
+          Oh no :(
+        </Text>
+      </Grid>
+      <Spacer h={2} />
+      <Grid xs={24} lg={10}>
+        <Text
+          style={{ textAlign: 'center' }}
+          className="font-standard"
+          font="20px"
+        >
+          {t('rsvp_error')}
+        </Text>
+      </Grid>
+      <Grid>
+        <Link href={`mailto:${email}`}>
+          <Text className="no-vertical-margin" font="14px">
+            {email}
+          </Text>
+        </Link>
+      </Grid>
+    </Grid.Container>
+  )
 }
