6e94b7fd0ca98cd1594735c2bcbcb130e206bc26
Marco Levrero <marco.levrero@gmail.com>
2022-02-06 15:29:28 +0100

    rsvp confirmed screen

--- src/main/pages/RSVPConfirmed.tsx
+++ src/main/pages/RSVPConfirmed.tsx
@@ -1,5 +1,33 @@
-import { Text } from '@geist-ui/react'
+import { Grid, Link, Text } from '@geist-ui/react'
+import { useTranslation } from 'react-i18next'
 
 export const RSVPConfirmed = () => {
-  return <Text>Confirmed</Text>
+  const email = 'oliviaemarcowedding@email.com'
+  const { t } = useTranslation()
+
+  return (
+    <Grid.Container style={{ alignItems: 'center', flexDirection: 'column' }}>
+      <Grid xs={24}>
+        <Text font="35px" className="font-standard">
+          {t('thank_you')}
+        </Text>
+      </Grid>
+      <Grid xs={24} lg={10}>
+        <Text
+          style={{ textAlign: 'center' }}
+          className="font-standard"
+          font="20px"
+        >
+          {t('rsvp_confirmed')}
+        </Text>
+      </Grid>
+      <Grid>
+        <Link href={`mailto:${email}`}>
+          <Text className="no-vertical-margin" font="14px">
+            {email}
+          </Text>
+        </Link>
+      </Grid>
+    </Grid.Container>
+  )
 }
