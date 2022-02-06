14a9423379ae720ac1fa6558e64f600cd4e1b21b
Marco Levrero <marco.levrero@gmail.com>
2022-02-06 15:29:13 +0100

    uppercase codes

--- src/main/pages/RSVP.tsx
+++ src/main/pages/RSVP.tsx
@@ -36,7 +36,7 @@ export const RSVP = (props: { mobileView: boolean }) => {
   const onSubmit = () => {
     modalLoading.setVisible(true)
 
-    loadParNWithTimeout(() => findPartyByCode(input.toLowerCase()), 1500, 20000)
+    loadParNWithTimeout(() => findPartyByCode(input.toUpperCase()), 1500, 20000)
       .then(
         fold(
           () => navigate('/rsvp/error'),
